b7109aff31ea74041e987e12de3979de17bee440
Diego Mazega <diego.mazega@live.com>
2020-02-18 19:49:38 -0300

    Criação da classe Position.

---
+++ src/application/Main.java
@@ -0,0 +1,10 @@
+package src.application;
+
+import src.boardgame.Position;
+
+class Main{
+    public static void main(String[] args) {
+      Position a = new Position(2, 3);
+      System.out.println(a);  
+    }
+}
---
+++ src/boardgame/Position.java
@@ -0,0 +1,22 @@
+package src.boardgame;
+
+public class Position{
+    private int row;
+    private int column;
+
+    public Position(int row, int column){
+        this.row = row;
+        this.column = column;
+    }
+
+    public int getRow(){return this.row;}
+    public int getColumn(){return this.column;}
+
+    public void setRow(int row){this.row = row;}
+    public void setColumns(int column){this.column = column;}
+
+    @Override
+    public String toString(){
+        return getRow() + " , " + getColumn();          
+    }
+}
