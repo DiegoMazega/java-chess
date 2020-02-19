90b02c17414b5f64eb516469b2c7f85337523819
Diego Mazega <diego.mazega@live.com>
2020-02-19 00:03:47 -0300

    classe para imprimir o tabuleiro na tela

---
+++ src/application/UI.java
@@ -0,0 +1,28 @@
+package src.application;
+
+import src.chess.ChessPiece;
+
+public class UI{
+    
+    //impimir as peças
+    public static void printBoard(ChessPiece[][] pieces){
+        for(int i = 0; i < pieces.length; i++){
+            System.out.print((8-i)+ " ");
+            for(int j = 0; j<pieces.length; j++){
+                printPiece(pieces[i][j]);
+            }
+            System.out.println();
+        }
+        System.out.println("  a b c d e f g h");
+    }
+
+
+    //imprimir uma peça
+    private static void printPiece(ChessPiece piece){
+        if(piece == null) System.out.print("-");
+        else System.out.print(piece);
+
+        System.out.print(" ");
+    }
+
+}
