817b614c2089e489cfb2b4bc29d322312ee46dc5
Diego Mazega <diego.mazega@live.com>
2020-02-18 23:20:58 -0300

    Criação inicial da classe Board

---
+++ src/boardgame/Board.java
@@ -0,0 +1,20 @@
+package src.boardgame;
+
+public class Board{
+    private int rows;
+    private int columns;
+    private Piece[][] pieces;
+
+    public Board(int rows, int columns){
+        this.rows = rows;
+        this.columns = columns;
+        this.pieces = new Piece[rows][columns];
+    }
+
+    public int getRows(){return this.rows;}
+    public int getColumns(){return this.columns;}
+
+    public void setRows(int rows){this.rows = rows;}
+    public void setColumns(int columns){this.columns = columns;}
+
+}
